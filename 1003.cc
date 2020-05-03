3ab9dc0636185cc213ddfdde6ddfab51af570274
MS269 <59796004+MS269@users.noreply.github.com>
2020-05-03 14:46:21 +0900

    05/03/20

---
+++ 1003.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+//#include <utility>
+//#include <string>
+//#include <algorithm>
+//#include <cmath>
+//#include <climits>
+
+using namespace std;
+
+//using ll = long long;
+
+const int kMax = 40;
+
+int main() {
+#define DEBUG
+#ifndef DEBUG
+#define DEBUG
+  FILE* stream;
+  freopen_s(&stream, "input.txt", "r", stdin);
+  freopen_s(&stream, "output.txt", "w", stdout);
+#endif // DEBUG
+
+//#define FASTIO
+#ifndef FASTIO
+#define FASTIO
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+#endif // FASTIO
+
+  int t;
+  cin >> t;
+  vector<int> memo(kMax + 1);
+  memo[0] = 0;
+  memo[1] = 1;
+  for (int i = 2; i <= kMax; i++)
+    memo[i] = memo[i - 1] + memo[i - 2];
+  int n;
+  while (t--) {
+    cin >> n;
+    if (n == 0)
+      cout << 1 << " " << 0 << "\n";
+    else
+      cout << memo[n - 1] << " " << memo[n] << "\n";
+  }
+
+  return 0;
+}
---
+++ 2748.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+//#include <string>
+//#include <utility>
+//#include <algorithm>
+//#include <cmath>
+//#include <climits>
+
+using namespace std;
+
+using ll = long long;
+
+int main() {
+#define DEBUG
+#ifndef DEBUG
+#define DEBUG
+  FILE* stream;
+  freopen_s(&stream, "input.txt", "r", stdin);
+  freopen_s(&stream, "output.txt", "w", stdout);
+#endif // DEBUG
+
+//#define FASTIO
+#ifndef FASTIO
+#define FASTIO
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+#endif // FASTIO
+
+  int n;
+  cin >> n;
+  vector<ll> memo(n + 1);
+  memo[0] = 0;
+  memo[1] = 1;
+  for (int i = 2; i <= n; i++)
+    memo[i] = memo[i - 1] + memo[i - 2];
+  cout << memo[n];
+
+  return 0;
+}
