b498a2dcc32dd7d249b40c222cd0f70325cb49ad
MS269 <59796004+MS269@users.noreply.github.com>
2020-05-18 20:38:12 +0900

    05/18/20

---
+++ 1931.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(pair<int, int> a, pair<int, int> b) {
+  if (a.second == b.second)
+    return a.first < b.first;
+  return a.second < b.second;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<pair<int, int>> conf(n);
+  for (int i = 0; i < n; ++i)
+    cin >> conf[i].first >> conf[i].second;
+  sort(conf.begin(), conf.end(), compare);
+  int cnt = 0;
+  int end = 0;
+  for (int i = 0; i < n; ++i) {
+    if (conf[i].first >= end) {
+      end = conf[i].second;
+      cnt++;
+    }
+  }
+  cout << cnt;
+
+  return 0;
+}
