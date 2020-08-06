408a66231d48037527da06338472b8cebfd9af52
MS269 <59796004+MS269@users.noreply.github.com>
2020-08-06 14:57:53 +0900

    08/06/20

---
+++ 1927.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+#ifndef FASTIO
+#define FASTIO
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+#endif // FASTIO
+
+  int n;
+  cin >> n;
+  priority_queue<int, vector<int>, greater<int>> pq;
+  for (int i = 0; i < n; ++i) {
+    int x;
+    cin >> x;
+    if (x == 0) {
+      if (pq.empty()) {
+        cout << "0\n";
+      } else {
+        cout << pq.top() << "\n";
+        pq.pop();
+      }
+    } else if (x > 0) {
+      pq.push(x);
+    }
+  }
+
+  return 0;
+}
