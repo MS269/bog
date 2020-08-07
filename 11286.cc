bd3e0cc220c520a6f6c1c920904e717644f46857
MS269 <59796004+MS269@users.noreply.github.com>
2020-08-07 11:44:08 +0900

    08/07/20

---
+++ 11286.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+  for (int i = 0; i < n; ++i) {
+    int x;
+    cin >> x;
+    if (x == 0) {
+      if (pq.empty()) {
+        cout << "0\n";
+      } else {
+        cout << pq.top().second << "\n";
+        pq.pop();
+      }
+    } else {
+      pq.push({abs(x), x});
+    }
+  }
+
+  return 0;
+}
